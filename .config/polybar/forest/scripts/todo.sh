5097002a585bd8129e5cab74c9fd0ee01a3b8c81
Vets Emmanuel <vets.emmanuel@gmail.com>
2022-04-20 17:21:14 +0200

    Cleanup todo.sh

--- .config/polybar/forest/scripts/todo.sh
+++ .config/polybar/forest/scripts/todo.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-TODO_PATH="/home/vetse/todos.md"
-BACKUP_PATH="/home/vetse/Notes/Railnova/Todos"
+BACKUP_PATH="/home/vetse/Notes/Railnova/Todos/"
+TODO_PATH="/home/vetse/Documents/insync/todos.md"
+
+[ ! -f "$TODO_PATH" ] && echo "ERROR" && exit 1
 
 function count() {
-    full=$(cat $TODO_PATH|grep -Pc "^\[[xX ]\]")
-    done=$(cat $TODO_PATH|grep -Pc "^\[[xX]\]")
-    projects=$(cat $TODO_PATH|grep -Pc "^[#]{2}")
+    full=$(grep -Pc "^\[[xX ]\]" <$TODO_PATH)
+    done=$(grep -Pc "^\[[xX]\]" <$TODO_PATH)
+    projects=$(grep -Pc "^[#]{2}" <$TODO_PATH)
 
-    if [ "$full" -eq 0 ]
-    then
+    if [ "$full" -eq 0 ]; then
         echo "%{F#f00}NO TODO%{F-} [${projects}]"
         return 0
     fi
 
-    if [ "$full" -eq "$done" ]
-    then
+    if [ "$full" -eq "$done" ]; then
         echo "%{F#0F0}DONE%{F-} [${projects}]"
         return 0
     fi
@@ -24,25 +24,26 @@ function count() {
     return 0
 }
 
-
 function edit() {
     konsole -e nvim $TODO_PATH
     exit 0
 }
 
 function backup() {
-    mv $TODO_PATH ${TODO_PATH}_
-    cat ${TODO_PATH}_ | grep -v "^\[xX\]" > $TODO_PATH
-    mv ${TODO_PATH}_ $BACKUP_PATH/todos_"$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+    cp -L $TODO_PATH "$BACKUP_PATH"/todos_"$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+    sed -i '/^\[[xX]\]/d' $TODO_PATH
+    sed -i '/^$/N;/^\n$/D' $TODO_PATH
     exit 0
 }
 
-# if [ -n "$TODO_PATH" ]
-# then
-#     touch $TODO_PATH
-# fi
-
-[ "$1" == "edit" ] && edit
-[ "$1" == "backup" ] && backup
-
-count
+case "${1}" in
+"edit")
+    edit
+    ;;
+"backup")
+    backup
+    ;;
+*)
+    count
+    ;;
+esac
